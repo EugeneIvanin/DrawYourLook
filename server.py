cddd6851bb6364e3a49e7181cd81a4486e4fd949
Eugene <34417631+EugeneIvanin@users.noreply.github.com>
2018-11-12 02:30:42 +0300

    Update server.py

--- server.py
+++ server.py
@@ -2,10 +2,12 @@ import web
 
 urls = ("/.*", "hello")
 app = web.application(urls, globals())
+render = web.template.render('templates/')
+
 
 class hello:
-    def GET(self):
-        return 'Hello, world!'
+    name = 'Eugene'    
+    return render.index(name)
 
 if __name__ == "__main__":
     app.run()
