79d311269e4149346ef9c3a977e17a8c978bf66d
Eugene <34417631+EugeneIvanin@users.noreply.github.com>
2018-11-12 03:21:56 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,14 +1,41 @@
 import web
 
-urls = ("/.*", "hello")
+web.config.debug = False
+
+urls = ("/", "start",
+        "/filters", "filters",
+        "/result", "result")
+
 app = web.application(urls, globals())
 render = web.template.render('templates/')
+session = web.session.Session(app, web.session.DiskStore('sessions'), initializer={'filter': 0})
 
 
-class hello:
+class start:
     def GET(self):
         name = 'Eugene'
         return render.start(name)
+    
+    def POST():
+        raise web.seeother('/filters')
+        
+class filters:
+    def GET(self):
+        
+        name = 'filters_page'
+        return render.start(name)
+    
+    def POST():
+        raise web.seeother('/result')
+        
+class result:
+    def GET(self):
+        name = 'result_page'
+        return render.start(name)
+    
+    def POST():
+        raise web.seeother('/start')
 
+        
 if __name__ == "__main__":
     app.run()
