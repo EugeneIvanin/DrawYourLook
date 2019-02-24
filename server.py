b1dfda789e6d04ae09b5f0adc3fc399f587c5edd
Eugene <34417631+EugeneIvanin@users.noreply.github.com>
2019-02-24 04:02:58 +0300

    Update server.py

--- server.py
+++ server.py
@@ -39,8 +39,7 @@ class upload:
 
     def POST(self):
         x = web.input(myfile={})
-        print(x, file=sys.stderr)
-        print(x.myfile.filename, file=sys.stderr)
+        print(type(x), file=sys.stderr)
         filedir = 'C:\\Users\\Mvideo\\Desktop\\Hackathon\\app\\DrawYourLook' # change this to the directory you want to store the file in.
         if 'myfile' in x: # to check if the file-object is created
             filepath=x.myfile.filename.replace('\\','/') # replaces the windows-style slashes with linux ones.
