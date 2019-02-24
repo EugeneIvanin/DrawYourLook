641f8f90ddbb8126f2cfe780d914cb855590e7de
Eugene <34417631+EugeneIvanin@users.noreply.github.com>
2019-02-24 05:18:55 +0300

    Update server.py

--- server.py
+++ server.py
@@ -86,14 +86,7 @@ urls = ('/', 'index')
 app = web.application(urls, globals())
 
 myform = form.Form( 
-    form.Textbox("boe"), 
-    form.Textbox("bax", 
-        form.notnull,
-        form.regexp('\d+', 'Must be a digit'),
-        form.Validator('Must be more than 5', lambda x:int(x)>5)),
-    form.Textarea('moe'),
-    form.Checkbox('curly'), 
-    form.Dropdown('french', ['mustard', 'fries', 'wine'])) 
+    form.Textbox("boe")) 
 
 class index: 
     def GET(self): 
@@ -104,12 +97,9 @@ class index:
 
     def POST(self): 
         form = myform() 
-        if not form.validates(): 
-            return render.formtest(form)
-        else:
-            # form.d.boe and form['boe'].value are equivalent ways of
-            # extracting the validated arguments from the form.
-            return "Grrreat success! boe: %s, bax: %s" % (form.d.boe, form['bax'].value)
+        # form.d.boe and form['boe'].value are equivalent ways of
+        # extracting the validated arguments from the form.
+        return "Grrreat success! boe: %s, bax: %s" % ( form['boe'].value)
 
 if __name__=="__main__":
     web.internalerror = web.debugerror
