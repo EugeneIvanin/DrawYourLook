f985cba80072dc8368598457fa69637fb464de9a
Eugene <34417631+EugeneIvanin@users.noreply.github.com>
2019-02-24 03:04:42 +0300

    Update server.py

--- server.py
+++ server.py
@@ -5,18 +5,13 @@ import subprocess
 
 web.config.debug = False
 web.config.session_parameters['cookie_path'] = '/'
-
-def dima_roma(data):
-        print(data)
-        sys.stdout.flush()
-        return data
         
-
 
 urls = ("/", "start",
         "/filters(.*)", "filters",
         "/result(.*)", "result",
-        "/process(.*)", "process")
+        "/process(.*)", "process",
+       "/upload", "upload")
 
 app = web.application(urls, globals())
 render = web.template.render('templates/', cache = False)
@@ -28,21 +23,40 @@ if web.config.get('_session') is None:
 else:
     session = web.config._session
 
+class Upload:
+    def GET(self):
+        web.header("Content-Type","text/html; charset=utf-8")
+        return """<html><head></head><body>
+<form method="POST" enctype="multipart/form-data" action="">
+<input type="file" name="myfile" />
+<br/>
+<input type="submit" />
+</form>
+</body></html>"""
+
+    def POST(self):
+        x = web.input(myfile={})
+        filedir = '/' # change this to the directory you want to store the file in.
+        if 'myfile' in x: # to check if the file-object is created
+            filepath=x.myfile.filename.replace('\\','/') # replaces the windows-style slashes with linux ones.
+            filename=filepath.split('/')[-1] # splits the and chooses the last part (the filename with extension)
+            fout = open(filedir +'/'+ filename,'w') # creates the file where the uploaded file should be stored
+            fout.write(x.myfile.file.read()) # writes the uploaded file to the newly created file.
+            fout.close() # closes the file, upload complete.
+        raise web.seeother('/upload')
+
 
 class start:
     def GET(self):
         return render.start()
         
 class filters:
-    def POST():
-        data = web.data() # you can get data use this method
-        answer = dima_roma(data)
-        return
-        
     def GET(self, url):
         if not session['origin_url'].startswith('http') or url != 'fromprocess':
             session['origin_url'] = "http://" + url
         return render.filters()
+
+
 
 class process:
     def GET(self, filter_url):
