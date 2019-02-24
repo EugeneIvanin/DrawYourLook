cb7946b24f7823eba370deaf831fac1b2860f352
Eugene <34417631+EugeneIvanin@users.noreply.github.com>
2019-02-24 03:17:53 +0300

    Update server.py

--- server.py
+++ server.py
@@ -41,7 +41,7 @@ class upload:
             filepath=x.myfile.filename.replace('\\','/') # replaces the windows-style slashes with linux ones.
             filename=filepath.split('/')[-1] # splits the and chooses the last part (the filename with extension)
             fout = open(filedir +'/'+ filename,'w') # creates the file where the uploaded file should be stored
-            fout.write(x.myfile.file.read()) # writes the uploaded file to the newly created file.
+            fout.write(x.myfile.file.read().decode('utf-8')) # writes the uploaded file to the newly created file.
             fout.close() # closes the file, upload complete.
         raise web.seeother('/upload')
 
